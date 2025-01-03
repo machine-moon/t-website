a0253ca5938cc6c326b1328470391be41a83563f
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-01-03 02:37:26 -0500

    conf changes, going to add testing

---
+++ config/jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+    env: {
+        node: true,
+    },
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
